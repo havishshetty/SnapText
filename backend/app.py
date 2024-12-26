f19654ec2f5a7cf230441367834be544fcc36a99
Havish Shetty <shettyhavish@gmail.com>
2024-12-26 23:22:54 +0530

    Backend changes

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,10 @@ from PIL import Image
 import pytesseract
 import google.generativeai as genai
 from io import BytesIO
+import pymongo
 from bson import ObjectId
 
-genai.configure(api_key="Please put something lol")
+genai.configure(api_key="OMG LOL")
 
 app = Flask(__name__)
 CORS(app)
@@ -20,7 +21,7 @@ def upload_image():
         extracted_text = pytesseract.image_to_string(img)
 
         model = genai.GenerativeModel("gemini-1.5-flash")
-        gemini_response = model.generate_content("Please Tell me the answer for this text you either sumamrize this or explain or tell the answer please use plain text only dont add any bold text ie text enclosed by ** ** "+extracted_text)
+        gemini_response = model.generate_content("Please Tell me the answer for this text you either explain in detail or tell the answer please use plain text only dont add any bold text ie text enclosed by ** ** If its code give the code"+extracted_text)
 
         return jsonify({"success": True, "result": gemini_response.text})
     except Exception as e:
